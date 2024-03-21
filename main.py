9c25dbcdee39ec7849d840e352670a4d51db3f11
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-03-21 09:52:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,27 @@
     La seva funcionalitat és obtenir les dades, processar-les per a obtenir les “paraules boges”
     i finalment mostrar-les per pantalla
 """
+#import openai
 
+#openai.api_key ="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"
 import crazy_words
-
+import data_source
 opcio = []
 def menu():
-    demanar = input("Chatgpt(C), Teclat(T), Link(L): ")
+    demanar = input("Chatgpt(C), Teclat(T), Server(S): ")
     if demanar == 'T' or demanar == 't':
         opcio.append("teclat")
-    elif demanar == "L" or demanar == 'l':
-        opcio.append("link")
+    elif demanar == "S" or demanar == 's':
+        opcio.append("server")
     elif demanar == "C" or demanar == 'c':
         opcio.append("chatgpt")
     else:
         print("Error, indica una de les opcions indicades.")
 def identificarmenu():
     if opcio[0] == "teclat":
-        crazy_words.Texto_Ordenado, crazy_words.frase = crazy_words.frase()
-        crazy_words.palabras = crazy_words.separar()
-        crazy_words.frase_Desordenado = crazy_words.juntar()
-        crazy_words.resultado()
-    elif opcio[0] == "link":
-        print("link")
+        data_source.getDataFromKeyboard()
+    elif opcio[0] == "server":
+        print("server")
         #getDataFromApiNinja
     elif opcio[0] == "chatgpt":
         print("chatgpt")
