8c5ebb12c144713f50ea114ea158ad2691037361
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-03-21 09:51:45 +0100

    Update crazy_words.py

--- crazy_words.py
+++ crazy_words.py
@@ -13,13 +13,14 @@
 #4 Devolvemos toddo el texto desordenado
 #5 Escribimos un "Traducido ordenado" y ahí mostrará el texto original que mandó el usuario
 
-import data_source
+
 import random
 cEspeciales = [".", ",", "?", ":", ";", "!", "'", "¡", "¿"]
+#letras=('a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z')
 Texto_Ordenado=[]
 palabras=[]
 frase_Desordenado=[]
-def frase():
+def introduirfrase():
     try:
         frase=input("Introduce el texto que quieres desordenar: ")
         frase_ordenada=frase.split()
@@ -29,7 +30,7 @@ def frase():
 def separar():
     for palabrades in Texto_Ordenado:
         if len(palabrades) >= 3:
-            if palabrades[-1] in frase and palabrades[-1] not in cEspeciales:
+            if palabrades[-1] in introduirfrase and palabrades[-1] not in cEspeciales:
                 sep=list(palabrades[1:-1])
                 random.shuffle(sep)
                 separado=palabrades[0]+''.join(sep)+palabrades[-1]
@@ -48,4 +49,5 @@ def juntar():
 def resultado():
     print(frase_Desordenado)
     print("Texto ordenado:")
-    print(frase)
+    print(introduirfrase)
+
